7fc1b09611c5d2bba3dfd39d3f8b7e53bc6f597a
Felix Nievelstein <fn@impacgroup.de>
2021-11-19 15:55:41 +0100

    adding plugin methods to javascript bridge;

--- www/barcodeScanner.js
+++ www/barcodeScanner.js
@@ -3,9 +3,16 @@
 // Empty constructor
 function MlBarcodeScanner() {}
 
-
 MlBarcodeScanner.prototype.open = function(codes, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'MlBarcodeScanner', 'open', [codes]);
+    cordova.exec(successCallback, errorCallback, 'MlBarcodeScanner', 'openScanner', [codes]);
+}
+
+MlBarcodeScanner.prototype.open = function(answer, errorCallback) {
+    cordova.exec(null, errorCallback, 'MlBarcodeScanner', 'validationResult', [answer]);
+}
+
+MlBarcodeScanner.prototype.setInfoScreen = function(info, errorCallback) {
+    cordova.exec(null, errorCallback, 'MlBarcodeScanner', 'setInfoScreen', [info]);
 }
 
 MlBarcodeScanner.install = function() {
